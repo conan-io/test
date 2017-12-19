f51a2ff2aa1e925d8ad35691dc407a8d992369da
Luis Martinez de Bartolome <lasote@gmail.com>
2017-12-19 17:40:22 +0100

    exclude when necessary

--- conan/external_tools/msys2_cygwin_test.py
+++ conan/external_tools/msys2_cygwin_test.py
@@ -7,15 +7,19 @@ from conan.conf import msys2_in_path
 from conan.test_regression.utils.base_exe import BaseExeTest, save_files, run
 from conans import tools
 from conans.model.ref import ConanFileReference
+from conans.model.version import Version
 from conans.paths import CONANFILE
 from conans.test.utils.cpp_test_files import cpp_hello_conan_files
 from conans.test.utils.tools import TestClient
 from conans.tools import unix_path
+from conans import __version__ as conan_version
 
 
 class RunInMsys2BashTest(BaseExeTest):
 
     def run_in_windows_bash_test(self):
+        if Version(conan_version) < Version("1.0.0-beta.1") or platform.system() != "Windows":
+            raise nose.SkipTest('Only windows test')
         with tools.remove_from_path("bash.exe"):
             with msys2_in_path():
                 conanfile = '''
@@ -39,6 +43,8 @@ class ConanBash(ConanFile):
                 self.assertIn(expected_curdir_base, client.user_io.out)
 
     def run_in_windows_bash_relative_path_test(self):
+        if Version(conan_version) < Version("1.0.0-beta.1") or platform.system() != "Windows":
+            raise nose.SkipTest('Only windows test')
         with tools.remove_from_path("bash.exe"):
             with msys2_in_path():
                 conanfile = '''
@@ -59,6 +65,8 @@ class ConanBash(ConanFile):
                 self.assertIn("build/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/relative", client.user_io.out)
 
     def run_in_windows_bash_env_var_test(self):
+        if Version(conan_version) < Version("1.0.0-beta.1") or platform.system() != "Windows":
+            raise nose.SkipTest('Only windows test')
         with tools.remove_from_path("bash.exe"):
             with msys2_in_path():
                 conanfile = '''
@@ -103,7 +111,7 @@ class MSys2CygwinTestBuildRequiresOrderAppliedPath(BaseExeTest):
     @parameterized.expand([("cygwin_installer/2.9.0@bincrafters/stable", ),
                            ("msys2_installer/latest@bincrafters/stable",)])
     def test_base(self, subsystem_require):
-        if platform.system() != "Windows":
+        if Version(conan_version) < Version("1.0.0-beta.1") or platform.system() != "Windows":
             raise nose.SkipTest('Only windows test')
         files = cpp_hello_conan_files(name="Hello", version="0.1", deps=None, language=0, static=True,
                                       use_cmake=False)
