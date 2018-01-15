e42e9ea611bb035aced3b64c9a96caefd86d9037
Luis Martinez de Bartolome <lasote@gmail.com>
2018-01-15 15:59:02 +0100

    More test of win in bash

--- conan/conf.py
+++ conan/conf.py
@@ -9,7 +9,9 @@ from contextlib import contextmanager
 CONAN_MINGW_PATH = os.getenv("CONAN_MINGW_PATH", "C:/Mingw/bin")
 CONAN_MSYS2_PATH = os.getenv("CONAN_MSYS2_PATH", "C:/msys64/usr/bin")
 CONAN_CYGWIN_PATH = os.getenv("CONAN_CYGWIN_PATH", "C:/cygwin64/bin")
-
+CONAN_WSL_PATH = os.getenv("CONAN_WSL_PATH", "c:/windows/sysnative")
+CONAN_GIT_BIN_PATH = os.getenv("CONAN_GIT_BIN_PATH", "C:/Tools/Git/bin")
+CONAN_GIT_BIN_PATH2 = os.getenv("CONAN_GIT_BIN_PATH2", "C:/Program Files/Git/bin")
 
 @contextmanager
 def mingw_in_path():
---
+++ conan/external_tools/run_in_windows_bash_test.py
@@ -0,0 +1,30 @@
+import os
+import unittest
+
+from conan.conf import CONAN_WSL_PATH, CONAN_MSYS2_PATH, CONAN_CYGWIN_PATH, CONAN_GIT_BIN_PATH, CONAN_GIT_BIN_PATH2
+from conans import tools
+from conans.test.utils.tools import TestClient
+
+
+class RunInWindowsBashTest(unittest.TestCase):
+
+    def test_run_bash_in_windows(self):
+        conanfile = """
+from conans import ConanFile, tools
+class LibConan(ConanFile):
+    def build(self):
+        self.run("echo 'hello Conan!'", win_bash=True)
+        self.run("ls", win_bash=True)
+"""
+
+        client = TestClient()
+        client.save({"conanfile.py": conanfile})
+        for bash_path in [CONAN_GIT_BIN_PATH, CONAN_GIT_BIN_PATH2, CONAN_MSYS2_PATH, CONAN_CYGWIN_PATH, CONAN_WSL_PATH]:
+            bash_path = os.path.normpath(os.path.join(bash_path, "bash.exe"))
+            if not os.path.exists(bash_path):
+                raise Exception("Not found tool: %s" % bash_path)
+
+            with tools.environment_append({"CONAN_BASH_PATH": bash_path}):
+                client.run("create . lib/1.0@lasote/stable")
+                self.assertIn(bash_path, client.out)
+                self.assertIn("hello Conan!", client.out)
