2b25a279c6185e4186b502daf25336222c394a66
Luis Martinez de Bartolome <luism@jfrog.com>
2018-01-16 11:51:46 +0100

    Only in windows filter

--- conan/external_tools/run_in_windows_bash_test.py
+++ conan/external_tools/run_in_windows_bash_test.py
@@ -1,14 +1,21 @@
 import os
+import platform
 import unittest
 
+import nose
+
 from conan.conf import CONAN_WSL_PATH, CONAN_MSYS2_PATH, CONAN_CYGWIN_PATH, CONAN_GIT_BIN_PATH, CONAN_GIT_BIN_PATH2
+from conans import __version__ as conan_version
 from conans import tools
+from conans.model.version import Version
 from conans.test.utils.tools import TestClient
 
 
 class RunInWindowsBashTest(unittest.TestCase):
 
     def test_run_bash_in_windows(self):
+        if Version(conan_version) < Version("1.0.0-beta.4") or platform.system() != "Windows":
+            raise nose.SkipTest('Only windows test')
         conanfile = """
 from conans import ConanFile, tools
 class LibConan(ConanFile):
