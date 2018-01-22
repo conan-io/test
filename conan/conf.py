7acb4ddfb5ebba6959288d1eb47c28e5e90c2a2c
Luis Martinez de Bartolome <lasote@gmail.com>
2018-01-22 13:03:57 +0100

    More test to run in bash

--- conan/conf.py
+++ conan/conf.py
@@ -7,11 +7,17 @@ from conans import tools
 from contextlib import contextmanager
 
 CONAN_MINGW_PATH = os.getenv("CONAN_MINGW_PATH", "C:/Mingw/bin")
+
 CONAN_MSYS2_PATH = os.getenv("CONAN_MSYS2_PATH", "C:/msys64/usr/bin")
+CONAN_MSYS2_PATH_SPACES = os.getenv("CONAN_MSYS2_PATH_SPACES", "C:/msys64/usr/b in")
+
 CONAN_CYGWIN_PATH = os.getenv("CONAN_CYGWIN_PATH", "C:/cygwin64/bin")
+CONAN_CYGWIN_PATH_SPACES = os.getenv("CONAN_CYGWIN_PATH_SPACES", "C:/cygwin64/b in")
+
 CONAN_WSL_PATH = os.getenv("CONAN_WSL_PATH", "c:/windows/sysnative")
+
 CONAN_GIT_BIN_PATH = os.getenv("CONAN_GIT_BIN_PATH", "C:/Tools/Git/bin")
-CONAN_GIT_BIN_PATH2 = os.getenv("CONAN_GIT_BIN_PATH2", "C:/Program Files/Git/bin")
+CONAN_GIT_BIN_PATH_SPACES = os.getenv("CONAN_GIT_BIN_PATH_SPACES", "C:/Program Files/Git/bin")
 
 @contextmanager
 def mingw_in_path():
--- conan/external_tools/run_in_windows_bash_test.py
+++ conan/external_tools/run_in_windows_bash_test.py
@@ -4,7 +4,8 @@ import unittest
 
 import nose
 
-from conan.conf import CONAN_WSL_PATH, CONAN_MSYS2_PATH, CONAN_CYGWIN_PATH, CONAN_GIT_BIN_PATH, CONAN_GIT_BIN_PATH2
+from conan.conf import CONAN_WSL_PATH, CONAN_MSYS2_PATH, CONAN_CYGWIN_PATH, CONAN_GIT_BIN_PATH, CONAN_GIT_BIN_PATH_SPACES, \
+    CONAN_MSYS2_PATH_SPACES, CONAN_CYGWIN_PATH_SPACES
 from conans import __version__ as conan_version
 from conans import tools
 from conans.model.version import Version
@@ -14,20 +15,30 @@ from conans.test.utils.tools import TestClient
 class RunInWindowsBashTest(unittest.TestCase):
 
     def test_run_bash_in_windows(self):
-        if Version(conan_version) < Version("1.0.2") or platform.system() != "Windows":
+        if Version(conan_version) < Version("1.0.3") or platform.system() != "Windows":
             raise nose.SkipTest('Only windows test')
         conanfile = """
-from conans import ConanFile, tools
+import os
+from conans import ConanFile, tools, AutoToolsBuildEnvironment
 class LibConan(ConanFile):
     def build(self):
         self.run("echo 'hello Conan!'", win_bash=True)
         self.run("ls", win_bash=True)
+        tools.save("configure", "echo 'configuring...'")
+        tools.save("Makefile", "all:\\n\\techo 'makeing...'")
+        abe = AutoToolsBuildEnvironment(self, win_bash=True)
+        abe.configure(configure_dir=self.build_folder, args=["--enable-silent-rules"])
+        abe.make()
 """
 
         client = TestClient()
         client.save({"conanfile.py": conanfile})
-        for bash_path in [CONAN_GIT_BIN_PATH, CONAN_GIT_BIN_PATH2, CONAN_MSYS2_PATH, CONAN_CYGWIN_PATH, CONAN_WSL_PATH]:
+        for bash_path in [CONAN_GIT_BIN_PATH, CONAN_GIT_BIN_PATH_SPACES,
+                          CONAN_MSYS2_PATH, CONAN_MSYS2_PATH_SPACES,
+                          CONAN_CYGWIN_PATH, CONAN_CYGWIN_PATH_SPACES,
+                          CONAN_WSL_PATH]:
             full_path = os.path.normpath(os.path.join(bash_path, "bash.exe"))
+            print("Trying %s" % full_path)
             # CI doesn't have WSL (not win10)
             if not os.path.exists(full_path):
                 if bash_path != CONAN_WSL_PATH:
@@ -38,4 +49,7 @@ class LibConan(ConanFile):
             with tools.environment_append({"CONAN_BASH_PATH": full_path}):
                 client.run("create . lib/1.0@lasote/stable")
                 self.assertIn(full_path, client.out)
+                print(client.out)
                 self.assertIn("hello Conan!", client.out)
+                self.assertIn("configuring...", client.out)
+                self.assertIn("makeing...", client.out)
