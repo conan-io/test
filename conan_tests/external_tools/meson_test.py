c3adcaaed3a4cde73d5357f821c875a5d1ec64cd
memsharded <james@conan.io>
2019-07-30 10:39:15 +0200

    fixing test

--- conan_tests/external_tools/meson_test.py
+++ conan_tests/external_tools/meson_test.py
@@ -1,5 +1,6 @@
 import os
 import platform
+import textwrap
 import unittest
 
 import six
@@ -15,34 +16,33 @@ from nose_parameterized import parameterized
 
 class PkgConfigGeneratorTest(unittest.TestCase):
 
+    
     @parameterized.expand([(True, ), (False, )])
+    @unittest.skipIf(sys.version_info[0] < 3 or sys.version_info[1] < 5, "meson > py 3.4")
     def basic_test(self, no_copy_source):
-
-        # meson > py 3.4
-        if sys.version_info[0] < 3 or sys.version_info[1] < 5:
-            return
-
         client = TestClient()
-        conanfile = """from conans import ConanFile, Meson, load
-import os
-class Conan(ConanFile):
-    settings = "os", "compiler", "arch", "build_type"
-    exports_sources = "src/*"
-    no_copy_source = {}
-    def build(self):
-        meson = Meson(self)
-        meson.configure(source_folder="src",
-                        cache_build_folder="build")
-        meson.build()
-    def package(self):
-        self.copy("*.h", src="src", dst="include")
+        conanfile = textwrap.dedent("""
+            from conans import ConanFile, Meson, load
+            import os
+            class Conan(ConanFile):
+                settings = "os", "compiler", "arch", "build_type"
+                exports_sources = "src/*"
+                no_copy_source = {}
+                def build(self):
+                    meson = Meson(self)
+                    meson.configure(source_folder="src",
+                                    cache_build_folder="build")
+                    meson.build()
+                def package(self):
+                    self.copy("*.h", src="src", dst="include")
 
-    def package_info(self):
-        self.output.info("HEADER %s" % load(os.path.join(self.package_folder, "include/header.h")))
-    """.format(no_copy_source)
-        meson = """project('hello', 'cpp', version : '0.1.0',
-		default_options : ['cpp_std=c++11'])
-"""
+                def package_info(self):
+                    self.output.info("HEADER %s" % load(os.path.join(self.package_folder, "include/header.h")))
+                """).format(no_copy_source)
+        meson = textwrap.dedent("""
+            project('hello', 'cpp', version : '0.1.0',
+		    default_options : ['cpp_std=c++11'])
+            """)
         client.save({"conanfile.py": conanfile,
                      "src/meson.build": meson,
                      "src/header.h": "//myheader.h"})
@@ -59,43 +59,39 @@ class Conan(ConanFile):
         self.assertTrue(os.path.exists(os.path.join(build_folder, "conanbuildinfo.txt")))
         self.assertEqual(load(os.path.join(build_folder, "package/include/header.h")), "//myheader.h")
 
+    @unittest.skipIf(sys.version_info[0] < 3 or sys.version_info[1] < 5, "meson > py 3.4")
     def test_base(self):
-
-        # meson > py 3.4
-        if sys.version_info[0] < 3 or sys.version_info[1] < 5:
-            return
-
         client = TestClient(path_with_spaces=False)
         self._export(client, "LIB_C", [])
         self._export(client, "LIB_B", ["LIB_C"])
         self._export(client, "LIB_B2", [])
         self._export(client, "LIB_A", ["LIB_B", "LIB_B2"])
 
-        consumer = """
-from conans import ConanFile, Meson
+        consumer = textwrap.dedent("""
+            from conans import ConanFile, Meson
 
-class ConanFileToolsTest(ConanFile):
-    generators = "pkg_config"
-    requires = "LIB_A/0.1@conan/stable"
-    settings = "os", "compiler", "build_type"
+            class ConanFileToolsTest(ConanFile):
+                generators = "pkg_config"
+                requires = "LIB_A/0.1@conan/stable"
+                settings = "os", "compiler", "build_type"
 
-    def build(self):
-        meson = Meson(self)
-        meson.configure()
-        meson.build()
-"""
-        meson_build = """
-project('conan_hello', 'c')
-liba = dependency('LIB_A', version : '>=0')
-executable('demo', 'main.c', dependencies: [liba])
-"""
+                def build(self):
+                    meson = Meson(self)
+                    meson.configure()
+                    meson.build()
+            """)
+        meson_build = textwrap.dedent("""
+            project('conan_hello', 'c')
+            liba = dependency('LIB_A', version : '>=0')
+            executable('demo', 'main.c', dependencies: [liba])
+            """)
 
-        main_c = """
-#include "helloLIB_A.h"
-int main(){
-  helloLIB_A();
-}
-"""
+        main_c = textwrap.dedent("""
+            #include "helloLIB_A.h"
+            int main(){
+            helloLIB_A();
+            }
+            """)
 
         client.save({CONANFILE: consumer,
                      "meson.build": meson_build,
@@ -104,15 +100,9 @@ int main(){
         client.current_folder = os.path.join(client.current_folder, "build")
         client.run("install .. --build")
 
-        if six.PY2:  # Meson only available
-            return
-
         client.run("build .. --source-folder ..")
-        if platform.system() == "Windows":
-            command = "demo"
-        else:
-            command = './demo'
-        client.runner(command, cwd=os.path.join(client.current_folder))
+        command = "demo" if platform.system() == "Windows" else "./demo"
+        client.run_command(command)
         self.assertEqual(['Hello LIB_A', 'Hello LIB_B', 'Hello LIB_C', 'Hello LIB_B2'],
                          str(client.user_io.out).splitlines()[-4:])
 
