2fe33c63a2b040082f7e98b32722801c06f46986
jgsogo <jgsogo@gmail.com>
2020-03-23 18:58:08 +0100

    keep using while

--- artifactory_tests/test_runner/launch.sh
+++ artifactory_tests/test_runner/launch.sh
@@ -10,7 +10,7 @@ done
 
 echo "Artifactory responded OK!"
 curl -uadmin:password -XGET $ARTIFACTORY_DEFAULT_URL/api/system/version
-curl -uadmin:password --output /dev/null -XPOST "http://artifactory:8081/artifactory/api/system/licenses" -H "Content-type: application/json" -d '{ "licenseKey" : "'"$ART_LICENSE"'"}'
+curl -uadmin:password --output /dev/null -XPOST "$ARTIFACTORY_DEFAULT_URL/api/system/licenses" -H "Content-type: application/json" -d '{ "licenseKey" : "'"$ART_LICENSE"'"}'
 
 echo "Let's clone Conan"
 git clone $CONAN_GIT_REPO conan_sources
