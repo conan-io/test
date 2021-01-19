436fe06d004db5302260cf7204425b8a6a85ffd4
czoido <mrgalleta@gmail.com>
2021-01-19 12:51:51 +0100

    install nose temporarily

--- artifactory_tests/test_runner/launch.sh
+++ artifactory_tests/test_runner/launch.sh
@@ -17,6 +17,6 @@ git clone $CONAN_GIT_REPO conan_sources
 cd conan_sources
 git checkout $CONAN_GIT_TAG
 echo "Let's install Conan as editable"
-pip3 install -e . && pip3 install -r conans/requirements_dev.txt
+pip3 install -e . && pip3 install -r conans/requirements_dev.txt && pip3 install nose
 echo "Let's run the tests"
 nosetests -w conans -A "artifactory_ready" -v
