5ac4911462225196f854a1adff97ea4eefdfb8e6
czoido <mrgalleta@gmail.com>
2023-05-25 17:37:16 +0200

    wip

--- artifactory_tests/run_tests.sh
+++ artifactory_tests/run_tests.sh
@@ -2,25 +2,18 @@
 
 set -e
 
-function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
-min_artifactory_version=6.9.0
-if version_gt $min_artifactory_version $ARTIFACTORY_VERSION; then
-     echo "Artifactory not supported for Conan revisions, exiting ok"
-     exit 0
-fi
+# echo "Building containers..."
+# docker-compose build
+# docker-compose pull
+# echo "Launching containers..."
+# docker-compose up -d
+# if docker-compose run test_runner ./launch.sh; then
+#     echo "Tests OK!"
+#     docker-compose down
+#     exit 0
+# else
+#     echo "Tests Failed!"
+#     docker-compose down
+#     exit 99
+# fi
 
-
-echo "Building containers..."
-docker-compose build
-docker-compose pull
-echo "Launching containers..."
-docker-compose up -d
-if docker-compose run test_runner ./launch.sh; then
-    echo "Tests OK!"
-    docker-compose down
-    exit 0
-else
-    echo "Tests Failed!"
-    docker-compose down
-    exit 99
-fi
