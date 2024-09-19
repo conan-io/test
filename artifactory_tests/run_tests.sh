68cb50d92fa36c0ebdefad85d9510963583ba8fe
Carlos Zoido <mrgalleta@gmail.com>
2024-09-19 13:11:56 +0200

    Fix tests running Artifactory versions > 7.63.14 (#65)
    * update docker compose
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * add latest
    
    * put loggin back
    
    * not latest
    
    * add latest
    
    * wip
    
    * wip
    
    * revert
    
    * fix copy
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * minor changes
    
    * simplify

--- artifactory_tests/run_tests.sh
+++ artifactory_tests/run_tests.sh
@@ -2,28 +2,34 @@
 
 set -e
 
+# Upgrade pip and install dependencies
+pip install --upgrade pip
 pip install docker-compose
-pip install docker==6.1.3
+pip install "docker==6.1.3"
 
-function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
-min_artifactory_version=6.9.0
-if version_gt $min_artifactory_version $ARTIFACTORY_VERSION; then
-     echo "Artifactory not supported for Conan revisions, exiting ok"
-     exit 0
+# Function to compare versions
+function version_gt() { 
+    test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"
+}
+
+min_artifactory_version="6.9.0"
+if version_gt "$min_artifactory_version" "$ARTIFACTORY_VERSION"; then
+    echo "Artifactory version $ARTIFACTORY_VERSION does not support Conan revisions, exiting successfully."
+    exit 0
 fi
 
-
-echo "Building containers..."
+echo "Building Docker containers..."
 docker-compose build
 docker-compose pull
-echo "Launching containers..."
+
+echo "Starting Docker containers and running tests..."
 docker-compose up -d
 if docker-compose run test_runner ./launch.sh; then
-    echo "Tests OK!"
+    echo "Tests passed!"
     docker-compose down
     exit 0
 else
-    echo "Tests Failed!"
+    echo "Tests failed or Artifactory failed to start."
     docker-compose down
     exit 99
 fi
