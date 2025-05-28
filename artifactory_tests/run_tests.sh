3cb2cfff37f9b693fcdfbaafb0335f9402d3a09c
Carlos Zoido <mrgalleta@gmail.com>
2025-05-28 15:53:54 +0200

    Use docker compose v2 for tests (#66)
    * wip
    
    * wip
    
    * use docker compose
    
    * wip
    
    * clean a bit

--- artifactory_tests/run_tests.sh
+++ artifactory_tests/run_tests.sh
@@ -1,35 +1,20 @@
 #!/usr/bin/env bash
-
 set -e
 
-# Upgrade pip and install dependencies
 pip install --upgrade pip
-pip install docker-compose
-pip install "docker==6.1.3"
-
-# Function to compare versions
-function version_gt() { 
-    test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"
-}
-
-min_artifactory_version="6.9.0"
-if version_gt "$min_artifactory_version" "$ARTIFACTORY_VERSION"; then
-    echo "Artifactory version $ARTIFACTORY_VERSION does not support Conan revisions, exiting successfully."
-    exit 0
-fi
 
 echo "Building Docker containers..."
-docker-compose build
-docker-compose pull
+docker compose build
+docker compose pull
 
 echo "Starting Docker containers and running tests..."
-docker-compose up -d
-if docker-compose run test_runner ./launch.sh; then
+docker compose up -d
+if docker compose run test_runner ./launch.sh; then
     echo "Tests passed!"
-    docker-compose down
+    docker compose down
     exit 0
 else
     echo "Tests failed or Artifactory failed to start."
-    docker-compose down
+    docker compose down
     exit 99
 fi
